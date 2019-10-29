ef99d2f9e9f5bc8d3a1f79b2bfdb9af7da702938
mjakobczyk <m.jakob@wp.pl>
2019-10-29 15:51:53 +0100

    Introduce more env vars

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     """Set Flask configuration vars from .env file."""
 
     # General
-    TESTING = environ["TESTING"]
+    # TESTING = environ["TESTING"]
     FLASK_DEBUG = environ["FLASK_DEBUG"]
-    SECRET_KEY = environ.get('SECRET_KEY')
+    # SECRET_KEY = environ.get('SECRET_KEY')
 
     # Database
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
--- start.sh
+++ start.sh
@@ -11,8 +11,20 @@ export FLASK_DEBUG=1
 # Config
 export APP_CONFIG_FILE=config.py
 
+# Testing
+export TESTING=False
+
+# Secret
+export SECRET_KEY=123
+
+# PostgreSQL
+export POSTGRES_URL=127.0.0.1:5432
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=password
+export POSTGRES_DB=postgres
+
 # Database
-export SQLALCHEMY_DATABASE_URI=...
+export SQLALCHEMY_DATABASE_URI=
 export SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 flask run --host=0.0.0.0
