cc5c4fe2eea7859b6e55760880b6720b5cb33425
mjakobczyk <m.jakob@wp.pl>
2019-11-04 15:14:48 +0100

    Add mocked responses

--- application/auth/routes.py
+++ application/auth/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 from flask_login import current_user
 from flask import current_app as app
-from flask import request
+from flask import request, jsonify, make_response
+
 
 # Blueprint Configuration
 auth_bp = Blueprint('auth_bp', __name__)
@@ -9,30 +10,44 @@ auth_bp = Blueprint('auth_bp', __name__)
 
 @auth_bp.route('/api/login', methods=['GET'])
 def login():
+    data = {
+        "message": "",
+    }
+
     # POST
     if request.method == 'POST':
-        return "Mocked /api/login POST"
+        data["message"]="Mocked POST /api/login"
+        return make_response(jsonify(data), 200)
 
     # GET
-    return "Mocked /login GET"
+    data["message"]="Mocked GET /api/login"
+    return make_response(jsonify(data), 200)
 
 @auth_bp.route('/api/register', methods=['GET', 'POST'])
 def register():
-    # POST
+    data = {
+        "message": "",
+    }
+
+    # # POST
     if request.method == 'POST':
-        username = request.args.get('user')
-        email = request.args.get('email')
-        if username and email:
-            new_user = User(username=username,
-                            email=email,
-                            created=dt.now(),
-                            bio="In West Philadelphia born and raised, on the playground is where I spent most of my days",
-                            admin=False)  # Create an instance of the User class
-            db.session.add(new_user)  # Adds new User record to database
-            db.session.commit()  # Commits all changes
-            return "Added user /api/register POST"
-        else:
-            return "Incorrect parameters /api/register POST"
+        data["message"]="Mocked POST /api/register"
+        return make_response(jsonify(data), 200)
+
+        # username = request.args.get('user')
+        # email = request.args.get('email')
+        # if username and email:
+        #     new_user = User(username=username,
+        #                     email=email,
+        #                     created=dt.now(),
+        #                     bio="In West Philadelphia born and raised, on the playground is where I spent most of my days",
+        #                     admin=False)  # Create an instance of the User class
+        #     db.session.add(new_user)  # Adds new User record to database
+        #     db.session.commit()  # Commits all changes
+        #     return "Added user /api/register POST"
+        # else:
+        #     return "Incorrect parameters /api/register POST"
 
     # GET
-    return "Mocked /api/register GET"
+    data["message"]="Mocked GET /api/register"
+    return make_response(jsonify(data), 200)
--- application/dashboard/routes.py
+++ application/dashboard/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 from flask_login import current_user
 from flask import current_app as app
 from flask_login import login_required
+from flask import request, jsonify, make_response
 
 # Blueprint Configuration
 main_bp = Blueprint('main_bp', __name__)
@@ -9,4 +10,9 @@ main_bp = Blueprint('main_bp', __name__)
 
 @main_bp.route('/', methods=['GET'])
 def dashboard():
-    return "Application dashboard"
+    data = {
+        "message": "Welcome to Friendship Diary application!",
+        "version": "1.0"
+    }
+
+    return make_response(jsonify(data), 200)
