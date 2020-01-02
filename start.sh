71bc8e5230d3b6fda20de5e1f28178eec7a40e8c
mjakobczyk <m.jakob@wp.pl>
2020-01-02 19:48:46 +0100

    Remove envs from Dockerfile and docker-compose, add support to .env file

--- start.sh
+++ start.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-# Flask
-# export FLASK_DEBUG=1
-# export FLASK_ENV=development
-# export FLASK_APP=startup.py
-
-# Config
-# export APP_CONFIG_FILE=config.py
-
-# Testing
-# export TESTING=False
-
-# Secret
-# export SECRET_KEY=123
-
-# PostgreSQL
-# export POSTGRES_URL=127.0.0.1:5432
-# export POSTGRES_USER=postgres
-# export POSTGRES_PASSWORD=password
-# export POSTGRES_DB=postgres
-
-# Database
-# export SQLALCHEMY_DATABASE_URI=None
-# export SQLALCHEMY_TRACK_MODIFICATIONS=False
+source .env
 
 echo Starting flask server...
 
